143e80a03e3228732fd7605a72b9af1da4b34a59
shubhanshu-dreamfolks <shubhanshu.chouhan@dreamfolks.in>
2024-06-28 13:18:18 +0530

    The type builtin: executable files

--- app/main.py
+++ app/main.py
@@ -1,6 +1,45 @@
 import sys
+import os
 
 supported_commands = ["exit", "echo", "type"]
+
+def command_lookup_in_path(command, path):
+    for root, dirs, files in os.walk(path):
+        if command in files:
+            return os.path.join(root, command)
+
+def type_command(args):
+    if len(args) == 0:
+        print("type: too few arguments")
+    elif len(args) > 1:
+        print("type: too many arguments")
+    elif args[0] in supported_commands:
+        print(f"{args[0]} is a shell builtin")
+    elif os.environ["PATH"]:
+        paths = os.environ["PATH"].split(":")
+        location = False
+        for path in paths:
+            location = command_lookup_in_path(args[0], path)
+            if location:
+                print(f"{args[0]} is {location}")
+                break
+        
+        if not location:
+            print(f"{args[0]}: not found")
+            
+    else:
+        print(f"{args[0]}: not found")
+
+def exit_command(args):
+    code = 0
+    if len(args) > 1:
+        print("exit: too many arguments")
+    elif len(args) == 1 and not args[0].isnumeric():
+        print("exit: argument must be a number")
+    elif len(args) == 1:
+        code = int(args[0])
+    sys.exit(code)
+
 def main():
     while True:
         sys.stdout.write("$ ")
@@ -18,25 +57,11 @@ def main():
         args = exp_arr[1:]
 
         if (command == "exit"):
-            code = 0
-            if len(args) > 1:
-                print("exit: too many arguments")
-            elif len(args) == 1 and not args[0].isnumeric():
-                print("exit: argument must be a number")
-            elif len(args) == 1:
-                code = int(args[0])
-            sys.exit(code)
+            exit_command(args)
         elif (command == "echo"):
             print(" ".join(args))
         elif (command == "type"):
-            if len(args) == 0:
-                print("type: too few arguments")
-            elif len(args) > 1:
-                print("type: too many arguments")
-            elif args[0] in supported_commands:
-                print(f"{args[0]} is a shell builtin")
-            else:
-                print(f"{args[0]}: not found")
+            type_command(args)
         else:
             print(f"{command}: command not found")
 
