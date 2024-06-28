ce08841bd3dea7b3617c03a5ad3cae58781d7292
shubhanshu-dreamfolks <shubhanshu.chouhan@dreamfolks.in>
2024-06-28 12:09:44 +0530

    REPL implementation

--- app/main.py
+++ app/main.py
@@ -2,21 +2,21 @@ import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-    # Wait for user input
-    exp = input()
+        # Wait for user input
+        exp = input()
 
-    if exp.strip() == "":
-        return
+        if exp.strip() == "":
+            return
 
-    exp_arr = exp.split()
+        exp_arr = exp.split()
 
-    command = exp_arr[0]
-    
-    print(f"{command}: command not found")
+        command = exp_arr[0]
+        
+        print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
