01297c76b0577dbec62819ecfe9af88587a7e885
shubhanshu-dreamfolks <shubhanshu.chouhan@dreamfolks.in>
2024-06-28 11:59:29 +0530

    Handle missing commands

--- app/main.py
+++ app/main.py
@@ -7,7 +7,16 @@ def main():
     sys.stdout.flush()
 
     # Wait for user input
-    input()
+    exp = input()
+
+    if exp.strip() == "":
+        return
+
+    exp_arr = exp.split()
+
+    command = exp_arr[0]
+    
+    print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
